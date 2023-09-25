44bdd0b1acf56d34bde769d95eed2f443fffdf18
satyasriDodla <143990452+satyasriDodla@users.noreply.github.com>
2023-09-25 12:04:45 +0530

    A2.java

---
+++ A2.java
@@ -0,0 +1,9 @@
+public class A2 {
+    static {
+        System.out.println("Inside static block");
+    }
+    public static void main(String args[]) {
+        System.out.println("Inside the main method");
+    }
+}
+    
