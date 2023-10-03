5c96470b0bfc63467c51a9fb424cfcaab040fb69
satyasriDodla <143990452+satyasriDodla@users.noreply.github.com>
2023-10-03 10:11:27 +0530

    Create Bike4.java

---
+++ Bike4.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class Bike4 {
+    final int speedlimit;
+    Bike4() {
+        speedlimit = 40;
+        System.out.println("speedlimit is "+speedlimit);
+    }
+    public static void main(String args[]) {
+        Bike4 b4 = new Bike4();
+    }
+}
